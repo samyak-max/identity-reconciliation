19be3445294a7d58577132b2bee42afde1718d03
smyk <samyakchoudhary9@gmail.com>
2025-06-18 17:45:19 +0530

    init

---
+++ src/test/java/co/bitespeed/identityreconciliation/IdentityReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.bitespeed.identityreconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconciliationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
